e2500b0f4cab83f92c2f5acac172f41e7c9efd8f
nitishkasana3175-dot <nitishkasana3175@gmail.com>
2026-09-23 14:31:20 +0000

    commit

---
+++ 0_1basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountid=201005
+let accountemail="nitish123@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+//accountid=2  //not allowed
+accountemail="nk@gmail.com"
+accountpassword="21212121"
+accountcity="bengaluru"
+//to print
+console.log(accountid);
+console.table([accountid,accountemail,accountpassword,accountcity])
+
