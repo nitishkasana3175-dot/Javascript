d0a0f8f3073ddc91dd71b0d7afaac1fff9ee0aeb
nitishkasana3175-dot <nitishkasana3175@gmail.com>
2026-09-23 15:17:49 +0000

    conversion operation

---
+++ 0_1basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="nitish"
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueinnumber=Number(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+let isloggedin="nitish"
+let isbooleanloggedin=Boolean(isloggedin)
+// console.log(isbooleanloggedin)
+//1=>true ; 0=>false
+//""=>false
+//"nitish"=>true
+let somenumber=33;
+let stringNumber=String(somenumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
