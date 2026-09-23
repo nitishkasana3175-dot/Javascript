f8947d0a5fbe276395ef92a170eca48b2a5cc1b6
nitishkasana3175-dot <nitishkasana3175@gmail.com>
2026-09-23 15:56:58 +0000

    operations

--- 0_1basics/03_conversionOperation.js
+++ 0_1basics/03_conversionOperation.js
@@ -17,3 +17,29 @@ let somenumber=33;
 let stringNumber=String(somenumber);
 console.log(stringNumber)
 console.log(typeof stringNumber)
+//*********operations****** */
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+let str1="hello"
+let str2="nitish"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3+4)*5%3)
+console.log(+true)
+console.log(+"")
+let num1,num2,num3
+num1=num2=mum3=2+2
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter);
