5e7130a5840fb72570c00b6924e3da2aeb2a12fd
nitishkasana3175-dot <nitishkasana3175@gmail.com>
2026-09-23 14:46:28 +0000

    new

--- 0_1basics/02_datatypes.js
+++ 0_1basics/02_datatypes.js
@@ -1,1 +1,19 @@
- 
+"use strict";
+console.log(3+3);
+console.log("nitish");
+let name="nitish"
+let age=20
+let isloggedin=false
+let state
+//types of datatypes
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof undefined)
+console.log(typeof null)
