83dec5e5cbd9e3b856a827c566cdc2ea73bbec73
nitishkasana3175-dot <nitishkasana3175@gmail.com>
2026-09-26 05:48:06 +0000

    coomit

---
+++ 0_1basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+//primitive
+//7 types:string,number,boolean,null,undefined,symbol,bigInt
+const score=100
+const scorevalue=100.3
+
+const isloggedin=false
+const outsidetemp=null
+let useremail;
+const id=symbol('123')
+const anotherid=symbol('123')
+console.log(id===anotherid)
+//const bignumber=34343434343434n
+
+
+//reference(Non Primitive)
+//array,objects,functions
+const heros=["shaktiman","nagraj","doga"]
+let myobj={
+    name:"nitish",
+    age:22,
+}
+const myFunction=function()
+{
+    console.log("hello world");
+}
+console.log(typeof anotherid);
